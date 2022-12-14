1c856a2f93a8ec32cf0beceb8df992b245dc7190
Valepa2911 <megp17102017@mail.ru>
2022-12-14 20:14:45 +0400

    сохран.

---
+++ 111111111/Program.cs
@@ -0,0 +1,33 @@
+﻿Console.WriteLine("Введите размер матрицы");
+int x = Convert.ToInt32(Console.ReadLine());
+int y = Convert.ToInt32(Console.ReadLine());
+int[,] mas = new int[x, y];
+//int W = mas.GetLength(0);
+//int Q = mas.GetLength(1);
+Console.WriteLine("Введите числа для столбцов и для строк через Enter");
+for (int i = 0; i < x; i++)
+{
+    for (int j = 0; j < y; j++)
+    {
+        mas[i, j] = Convert.ToInt32(Console.ReadLine());
+    }
+}
+for (int i = 0; i < x; i++)
+{
+    for (int j = 0; j < y; j++)
+    {
+        Console.Write(mas[i, j] + "|");
+    }
+    Console.WriteLine();
+}
+
+
+for (int i = 0; i < x; i++)
+{
+    double sum = 0;
+    for (int j = 0; j < y; j++)
+    {
+        sum += mas[j, i];
+    }
+    Console.WriteLine("Сумма столбца {1}", i, sum/y);
+}
