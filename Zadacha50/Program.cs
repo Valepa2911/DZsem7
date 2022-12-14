ef745cde526353bd3b5b4e0647a98018530115e0
Valepa2911 <megp17102017@mail.ru>
2022-12-14 22:13:29 +0400

    сохран.

--- Zadacha50/Program.cs
+++ Zadacha50/Program.cs
@@ -7,4 +7,20 @@
 //17 -> такого числа в массиве нет
 
 Console.WriteLine("Введите число из массива: ");
-int[,] array2d = new int[,] {{21,33,15,65,-98,41},{65,36,30,15,-42,74}};
+var n = Convert.ToInt32(Console.ReadLine());
+int[,] array2d = new int[,] { { 21, 33, 15, 65, -98, 41 }, { 65, 36, 30, 15, -42, 74 } };
+Console.WriteLine(String.Join(" | ", array2d.Cast<int>()));
+for (int i = 0; i < array2d.GetLength(0); i++)
+{
+    for (int j = 0; j < array2d.GetLength(1); j++)
+    {
+        if (n == array2d[i, j])
+        {
+             Console.Write($"{i},{j} " + " ");
+        }
+        else
+        {
+            Console.Write("-" + " ");
+        }
+    }
+}
